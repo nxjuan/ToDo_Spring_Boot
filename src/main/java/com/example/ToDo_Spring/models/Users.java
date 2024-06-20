54d9706b09331337bfeb66df2c39334052cbacd9
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-06-19 23:17:59 -0300

    Projeto ajustado ao curso

--- src/main/java/com/example/ToDo_Spring/models/Users.java
+++ src/main/java/com/example/ToDo_Spring/models/Users.java
@@ -30,21 +30,20 @@ public class Users implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
+
     @Column(name = "username", length = 100, nullable = false, unique = true)
     @NotNull(groups = CreateUser.class)
     @NotEmpty(groups = CreateUser.class)
     @Size(groups = CreateUser.class, min = 2, max = 100)
     private String name;
+
     @Column(name = "email", length = 50, nullable = false, unique = true)
     @NotNull(groups = CreateUser.class)
     @NotEmpty(groups = CreateUser.class)
     @Size(groups = CreateUser.class, min = 6, max = 100)
     private String email;
-    @Column(name = "phone", length = 11, nullable = false, unique = true)
-    @NotNull(groups = {CreateUser.class, UpdateUser.class})
-    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
-    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 11)
-    private String phone;
+
+
     @Column(name = "password", length = 60, nullable = false)
     @NotNull(groups = {CreateUser.class, UpdateUser.class})
     @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
